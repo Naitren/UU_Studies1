63e679554f063845ef34d26e52e8bb9210341d93
Naitren <horrorkristi@gmail.com>
2024-10-16 16:31:02 +0700

    Add files via upload

---
+++ Practical work.py
@@ -0,0 +1,15 @@
+print ('task1') # 1st program
+print(9 ** 0.5 * 5)
+
+print('task2') # 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print('task3') # 3st program
+print(2 * 2 + 2)
+print( 2 * (2 + 2))
+print( 2 * 2 + 2 == 2 * (2 +2))
+
+print('task4') # 4st program
+print('123.456')
+print(float('123.456') * 10)
+print(int((int(1234.56) - 1194) / 10))
